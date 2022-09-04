48583d59fe7de5996ffd26da1aed340b6288c16a
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-09-04 19:28:53 -0400

    creates seed route

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,3 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
+
+// Seed route
+router.get('/seed', (req, res) => {
+    const data = require('../data.json');
+
+    Post.deleteMany({}, (err, result) => {
+        Post.insertMany(data, (err, result) => {
+            res.send(data);
+        });
+    });
+});
+
+module.exports = router;
