9a982d35dd1bd873bbe759baaf0474c4771f1f2b
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-09-04 19:29:08 -0400

    fixes order of schemas

--- models/post.js
+++ models/post.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Reply Schema
+const replySchema = new Schema ({
+    content: {type: String, required: true},
+}, {timestamps: true});
+
 // Post Schema
 const postSchema = new Schema({
     content: {type: String, required: true},
     replies: [replySchema],
-}, {timestamps: true});
-
-// Reply Schema
-const replySchema = new Schema ({
-    content: {type: String, required: true},
 }, {timestamps: true});
 
 module.exports = mongoose.model('Post', postSchema);
