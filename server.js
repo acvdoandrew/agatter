2d42e27a6cf6b680a02d612aa3a342b0d9ce8e07
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-09-06 14:43:56 -0400

    sets session middleware up

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const postsRouter = require('./controllers/posts');
+const usersRouter = require('./controllers/users');
+const expressSession = require('express-session');
 // Initialize the app
 const app = express();
 
@@ -25,12 +27,18 @@ app.use(express.urlencoded({ extended: false}));
 app.use(methodOverride('_method'));
 
 // Session middleware
+app.use(expressSession({
+    secret: 'safae3@#$%ssda',
+    resave: false,
+    saveUninitialized: false
+}));
 
 // Homepage route
 app.get('/', (req, res) => {
     res.redirect('/posts');
 });
 
+app.use(usersRouter);
 app.use(postsRouter);
 
 // Tell the app to listen
