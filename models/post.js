3fceec68ec24b9d3b4a8629fa0465e2b7f8e44ad
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-09-06 14:39:04 -0400

    adds addedBy field on Schemas

--- models/post.js
+++ models/post.js
@@ -4,12 +4,14 @@ const Schema = mongoose.Schema;
 // Reply Schema
 const replySchema = new Schema ({
     content: {type: String, required: true},
+    addedBy: {type: Schema.Types.ObjectId, ref: 'User'}
 }, {timestamps: true});
 
 // Post Schema
 const postSchema = new Schema({
     content: {type: String, required: true},
     replies: [replySchema],
+    addedBy: {type: Schema.Types.ObjectId, ref: 'User'}
 }, {timestamps: true});
 
 module.exports = mongoose.model('Post', postSchema);
