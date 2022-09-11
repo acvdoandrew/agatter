e69e58d0119f46d4b89d05df52ebe943c50c76f2
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-09-11 19:36:26 -0400

    gets rid of development seed route

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,17 +3,6 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
 
-// Seed route
-router.get('/seed', (req, res) => {
-    const data = require('../data.json');
-
-    Post.deleteMany({}, (err, result) => {
-        Post.insertMany(data, (err, result) => {
-            res.send(data);
-        });
-    });
-});
-
 // INDUCES
 // Index Route
 router.get('/', (req, res) => {
